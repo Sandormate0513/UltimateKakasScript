2a2fd884eb0abb59f306240bfee82ed857d209be
Mate <31538353+Sandormate0513@users.noreply.github.com>
2023-10-05 09:25:26 +0200

    beta 0.2.2
    Ha nincs még kakas mappa, létrehozza azt

--- kakas.bat
+++ kakas.bat
@@ -1,5 +1,7 @@
 echo kakas program futás alatt ...
 @echo off
+C:
+mkdir %appdata%\kakas
 cd %appdata%\kakas
 curl https://lh3.googleusercontent.com/drive-viewer/AK7aPaD6suUtk6xSPorgaoaApazp__-X-7DCA78yyBifvJkE-b0iQcDaupxpQSwKPvwREyODF4wtDDQRWHkx7aGt4fcDcHMltA=s1600 -o BlackCock.png
 reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d %appdata%\kakas\BlackCock.png /f 
@@ -13,6 +15,3 @@ echo RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True >> kakasexten
 cls
 echo Kakas sikeresen települt!
 exit
-
-
-
