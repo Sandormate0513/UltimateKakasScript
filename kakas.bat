03218793bc79522f66f56b91dbf8d2938cb052ee
Mate <31538353+Sandormate0513@users.noreply.github.com>
2025-10-14 22:30:37 +0200

    Update kakas.bat
    Changed the link so the picture is now downloaded from github

--- kakas.bat
+++ kakas.bat
@@ -2,7 +2,7 @@
 C:
 mkdir %appdata%\kakas
 cd %appdata%\kakas
-curl https://lh3.googleusercontent.com/drive-viewer/AK7aPaD6suUtk6xSPorgaoaApazp__-X-7DCA78yyBifvJkE-b0iQcDaupxpQSwKPvwREyODF4wtDDQRWHkx7aGt4fcDcHMltA=s1600 -o BlackCock.png
+curl https://raw.githubusercontent.com/Sandormate0513/UltimateKakasScript/refs/heads/main/BlackCock.png -o BlackCock.png
 reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d %appdata%\kakas\BlackCock.png /f 
 reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f 
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
